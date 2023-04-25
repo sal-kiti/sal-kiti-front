fea1302cb825334b1d8b621cfd008a54b3b24663
Jukka Karvonen <jukka.karvonen@iki.fi>
2023-04-25 17:56:58 +0300

    Sort results by custom field

--- src/utils/SortByPosition.js
+++ src/utils/SortByPosition.js
@@ -2,14 +2,15 @@
  * Sort results array by position, setting null positions last
  *
  * @param {array} array of results
+ * @param {string} field custom field for sorting
  * @returns {array}
  */
-export default function (array) {
+export default function (array, field = "position") {
   return array.sort(function (a, b) {
     return (
       (a.result === null) - (b.result === null) ||
-      +(a.position > b.position) ||
-      -(a.position < b.position)
+      +(a[field] > b[field]) ||
+      -(a[field] < b[field])
     );
   });
 }
